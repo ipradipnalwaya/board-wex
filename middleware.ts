39c714c5968ad543b9ea80bc37faf649131cdc1e
Pradip Nalwaya <pradipnetwork007@gmail.com>
2024-07-27 13:20:57 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,9 @@
-import { withClerkMiddleware } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// Use a custom afterAuth function to control behavior after authentication
-export default withClerkMiddleware((req) => {
-  return NextResponse.next();
-}, {
-  afterAuth(auth, req, evt) {
-    // Custom logic based on authentication status
-    if (!auth.userId) {
-      // Allow access to the home page even if not authenticated
-      if (req.nextUrl.pathname === "/") {
-        return NextResponse.next();
-      }
-      // Redirect unauthenticated users to a sign-in page or other location
-      return NextResponse.redirect("/sign-in");
-    }
-  }
+const isProtectedRoute = createRouteMatcher(["/(.*)", "/"]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
 });
 
 export const config = {
