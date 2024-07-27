79a5ff0d989cfbc19bc44ab74b970614d66b4e35
Pradip Nalwaya <pradipnetwork007@gmail.com>
2024-07-27 13:33:43 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,11 @@
-import { withClerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
 
-export default withClerkMiddleware((req, res, next) => {
-  // Your logic here
-  const isProtected = req.url.startsWith("/api") || req.url === "/";
-  if (isProtected) {
-    // Check authentication
-    if (!req.auth.userId) {
-      return res.status(401).json({ error: "Unauthorized" });
-    }
-  }
-  // Proceed to the next middleware or handler
-  next();
-});
+const isProtectedRoute = createRouteMatcher(["/"])
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect()
+})
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+}
