6fbd3ac1e7a70c45e02d718f3c21e226ba5cb259
Pradip Nalwaya <pradipnetwork007@gmail.com>
2024-07-27 13:01:14 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,13 @@
 import { withClerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 // This middleware function will enforce authentication on the specified routes
-export default withClerkMiddleware((req, res, next) => {
-  // Apply custom logic if needed before calling next()
-  next();
+export default withClerkMiddleware((req) => {
+  // Custom logic can be added here if needed
+  console.log("Request URL:", req.url);
+
+  // Continue to the next middleware or route handler
+  return NextResponse.next();
 });
 
 export const config = {
