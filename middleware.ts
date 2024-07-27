ddf5a49639eb0e2ebd4206e1d4b4c08657978b1e
Pradip Nalwaya <pradipnetwork007@gmail.com>
2024-07-27 13:59:44 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,8 @@
-import { authMiddleware, clerkMiddleware, createRouteMatcher  } from "@clerk/nextjs";
+import { authMiddleware } from "@clerk/nextjs";
  
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)']);
-
-export default clerkMiddleware((auth, request) => {
-  if(!isPublicRoute(request)) {
-    auth().protect();
-  }
-});
-
 export default authMiddleware({});
  
 export const config = {
