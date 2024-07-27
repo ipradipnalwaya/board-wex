6f9af7e907e4d3b5cd93bbd6c6dd3f9ab2dc6a34
Pradip Nalwaya <pradipnetwork007@gmail.com>
2024-07-27 13:55:32 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,8 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { authMiddleware, clerkMiddleware, createRouteMatcher  } from "@clerk/nextjs";
+ 
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)']);
 
@@ -8,11 +12,8 @@ export default clerkMiddleware((auth, request) => {
   }
 });
 
+export default authMiddleware({});
+ 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
